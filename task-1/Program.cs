7f59b4aa1ee083048117b5304a5b5915f66ddf36
Harish G <harishgovind6000@gmail.com>
2025-04-07 15:27:43 +0530

    completed task 1

--- task-1/Program.cs
+++ task-1/Program.cs
@@ -2,11 +2,34 @@
 
 namespace Task1
 {
+    class MathOperations
+    {
+        public static int Fact(int num)
+        {
+            if (num==0||num==1)
+            {
+                return 1;
+            }
+            else
+            {
+                return num*Fact(num-1);
+            }
+        }
+    }
     class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Console.Write("Enter a number to find its factorial:");
+            string number=Console.ReadLine();
+            int num=int.Parse(number);
+            if(num<0){
+                Console.WriteLine("Error: Factorial of negative number is not defined.");
+            }
+            else{
+                Console.WriteLine("Factorial of "+num+" is "+MathOperations.Fact(num));
+            }
+            
         }
     }
 }
